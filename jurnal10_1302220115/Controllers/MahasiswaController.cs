a96c8b3e1566b67a3b12371e779a57d1994dfcad
rakeanJP <rakean321@gmail.com>
2024-05-02 10:36:34 +0700

    menambahkan API

---
+++ jurnal10_1302220115/Controllers/MahasiswaController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace jurnal10_1302220115.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class MahasiswaController : Controller
+    {
+        public static List<Mahasiswa> dataMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa("Muhammad Rakean Ganteng","1302220115",["KPL","PBO","Basis Data"],2022),
+            new Mahasiswa("Jati Permana","1302220115",["KPL","PBO","Basis Data"],2022),
+            new Mahasiswa("Akbar Ganteng","1302220115",["KPL","PBO","Basis Data"],2022),
+            new Mahasiswa("kiki koko","1302220115",["KPL","PBO","Basis Data"],2022),
+            new Mahasiswa("Kezia Zahara","1302220115",["KPL","PBO","Basis Data"],2023),
+            new Mahasiswa("Zahra tiada dua","1302220115",["KPL","PBO","Basis Data"],2022)
+
+        };
+        [HttpGet]
+        public IEnumerable <Mahasiswa>Get()
+        {
+            return dataMahasiswa;
+        }
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return dataMahasiswa[id];
+        }
+        [HttpPost]
+        public void Post(Mahasiswa value)
+        {
+            dataMahasiswa.Add(value);
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            dataMahasiswa.RemoveAt(id);
+        }
+    }
+}
---
+++ jurnal10_1302220115/Mahasiswa.cs
@@ -0,0 +1,18 @@
+﻿namespace jurnal10_1302220115
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string nim { get; set; }
+        public List<string> Course { get; set; }  
+        
+        public int Year { get; set; }  
+        
+        public Mahasiswa(String nama, String nim,List<String>Course, int Year) {
+            this.Nama = nama;
+            this.nim = nim;
+            this.Course = Course;
+            this.Year = Year;
+        }  
+    }
+}
